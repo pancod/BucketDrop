6b6d20507bd39750162394257d117359548e4e7f
Patrick Ganss <patrick.ganss@gmx.de>
2016-08-25 15:23:44 +0200

    Add realm database and add drops dialog

---
+++ app/src/main/java/slidenerd/vivz/bucketdrops/DialogAdd.java
@@ -0,0 +1,7 @@
+package slidenerd.vivz.bucketdrops;
+
+/**
+ * Created by Patrick on 23.08.2016.
+ */
+public class DialogAdd {
+}
---
+++ app/src/main/java/slidenerd/vivz/bucketdrops/beans/Drop.java
@@ -0,0 +1,7 @@
+package slidenerd.vivz.bucketdrops.beans;
+
+/**
+ * Created by Patrick on 24.08.2016.
+ */
+public class Drop {
+}
